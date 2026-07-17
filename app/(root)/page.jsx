8a4c2e2ad7a8bef4e9b1f9ee8e293b610ed7245e
unknown <gerardmedano@gmail.com>
2026-07-17 13:49:32 +0100

    Cambiar About Me a Sobre mí

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -13,7 +13,7 @@ import { FullPageWrapper, Section, useFullPage } from "@alvalens/react-fullpage-
 import Button from "@/components/Button";
 import Me from "@/public/image/me/gerard.jpg";
 import MeAbout from "@/public/image/me/gerard.jpg";
-import Setup from "@/pubabout melic/image/projects/automation/leads/leads-diagram.png";
+import Setup from "@/public/image/projects/automation/leads/leads-diagram.png";
 import ProjectAll from "@/public/image/projects/automation/appsheet/appsheet-cover.jpg";
 import Hr from "@/components/Hr";
 // icons
